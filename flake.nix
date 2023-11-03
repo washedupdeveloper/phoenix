2f472d7d4124f3c54373a58a508f3985d0bd7fb0
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-11-03 10:43:28 +0100

    removed unnecessary flake inputs.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    nix-colors.url = "github:misterio77/nix-colors";
-    nur.url = "github:nix-community/NUR";
     nil.url = "github:oxalica/nil";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
