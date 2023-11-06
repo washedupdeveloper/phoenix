631e5aea86bbd22ef6cef58db2340edda3014092
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-11-06 19:32:06 +0100

    fixed typo

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   outputs = { self, ... } @ inputs:
-  let variables = import (if builtins.pathExists ./variables.nix then ./variables.nix else ./examples.variables.nix);
+  let variables = import (if builtins.pathExists ./variables.nix then ./variables.nix else ./example.variables.nix);
   in {
     nixosConfigurations = import ./hosts
       { inherit self inputs variables; }
