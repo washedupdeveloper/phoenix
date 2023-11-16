fac5d18c9ee1b1e0f6661462ff46a940e30a323f
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2023-11-16 03:17:08 +0100

    removed example variables

---
+++ variables.nix
@@ -0,0 +1,19 @@
+{
+  system = {
+    architecture = "x86_64-linux"; # e.g. "x86_64-linux"
+    username = "storm";
+    hostname = "nixos";
+    stateVersion = "23.05"; # e.g. "23.11"
+  };
+
+  git = {
+    username = "Storm";
+    email = "31986015+washedupdeveloper@users.noreply.github.com";
+    github.username = "washedupdeveloper";
+  };
+
+  region = {
+    timeZone = "Europe/Copenhagen"; # e.g. "UTC"
+    locale = "en_DK.UTF-8"; # e.g. "en_US.UTF-8"
+  };
+}
