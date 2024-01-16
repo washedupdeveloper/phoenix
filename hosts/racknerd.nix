473bf91195cfea6d5b5b4952af32f505db0a6ca0
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-01-16 14:39:02 +0100

    added hardware config to vps

--- hosts/racknerd.nix
+++ hosts/racknerd.nix
@@ -2,9 +2,12 @@
   lib,
   pkgs,
   config,
+  modulesPath,
   username,
   ...
 }: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+
   # overwrites, set by default in modules/system.nix
   networking.hostName = lib.mkForce "nixos-racknerd";
   time.timeZone = lib.mkForce "UTC";
@@ -19,4 +22,21 @@
     hashedPasswordFile = config.sops.secrets.user_password.path;
     openssh.authorizedKeys.keys = [config.sops.secrets.ssh_key_pub.path];
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [config.sops.secrets.ssh_key_pub.path];
+
+  boot = {
+    cleanTmpDir = true;
+    loader.grub.device = "/dev/vda";
+    initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
+    initrd.kernelModules = ["nvme"];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/vda2";
+    fsType = "ext4";
+  };
+
+  zramSwap.enable = false;
+  swapDevices = [{device = "/dev/vda3";}];
 }
