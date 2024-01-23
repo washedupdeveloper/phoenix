baab7a2cca3d2bcb337376d34d22d8631af286ae
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-01-23 23:29:30 +0100

    UNDO COMMIT

---
+++ modules/k3s/default.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  username,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [k3s];
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      # "--kubelet-arg=v=4" # Optional args
+    ];
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [6443];
+    allowedUDPPorts = [];
+  };
+
+  # systemd.tmpfiles.rules = let
+  #   permissionUserGroup = "0700 ${username} users";
+  # in
+  #   map (
+  #     file: "C /var/lib/rancher/k3s/server/manifests/${file}.yaml ${permissionUserGroup} - ${
+  #       (pkgs.formats.yaml {}).generate "" (import ./manifests/${file}.nix {})
+  #     }"
+  #   ) [
+  #   ];
+
+  systemd.tmpfiles.rules = let
+    permissionUserGroup = "0700 ${username} users";
+  in
+    map (
+      file: "C /var/lib/rancher/k3s/server/manifests/${file}.yaml ${permissionUserGroup} - ./manifests/${file}.yaml"
+    ) [
+      "traefik-dashboard"
+    ];
+}
