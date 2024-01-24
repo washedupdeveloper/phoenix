ada34755a4696cf4feeac8d04066334f92eff788
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-01-24 21:55:06 +0100

    removed accidental duplicate attribute

--- hosts/racknerd.nix
+++ hosts/racknerd.nix
@@ -15,11 +15,6 @@
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBCMD78tzMBKjffq9l65ho/6SDUrZu2gXeA6EpU5U/l 31986015+washedupdeveloper@users.noreply.github.com"];
   };
 
-  users.users.${username} = {
-    hashedPasswordFile = config.sops.secrets.user_password.path;
-    openssh.authorizedKeys.keys = [config.sops.secrets.ssh_key_pub.path];
-  };
-
   users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBCMD78tzMBKjffq9l65ho/6SDUrZu2gXeA6EpU5U/l 31986015+washedupdeveloper@users.noreply.github.com"];
 
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
