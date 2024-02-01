a27573b0423c88ea66d182fc98be09dcfa792718
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-02-01 17:40:41 +0100

    moved liveIso configuration into nixosconfigurations directly

--- modules/flake/nixosConfigurations.nix
+++ modules/flake/nixosConfigurations.nix
@@ -37,18 +37,30 @@ in {
       inputs.disko.nixosModules.disko
       ../nixos/k3s
       ../../hosts/laptop.nix
-      (import ../nixos/disko.nix {
-        device = "/dev/nvme0n1";
-        swapSizeInGb = "12";
-      })
     ];
     rpi = systemConfig "aarch64-linux" [
       "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
       ../../hosts/rpi.nix
     ];
-    racknerd = systemConfig "x86_64-linux" [../../hosts/racknerd.nix];
+    racknerd = systemConfig "x86_64-linux" [
+      inputs.disko.nixosModules.disko
+      ../../hosts/racknerd.nix
+    ];
     liveISO = systemConfig "x86_64-linux" [
-      ../nixos/liveIso.nix
+      {
+        services.openssh.settings.PasswordAuthentication = lib.mkForce true;
+        users.users.root = {
+          initialHashedPassword = lib.mkForce "$y$j9T$f4LE30RF2QMBy6jiR5j3M1$/X6daMyAm0fJ9iohebi0LZjiCHrmK092WpBpdTW6Z7A";
+          openssh.authorizedKeys.keys = [self.sshPubKey];
+          # TODO: add AGE secret for SOPS
+        };
+
+        systemd.services.cp-disko = {
+          script = "${pkgs.coreutils}/bin/cp ${self}/modules/nixos/disko.nix /tmp/disko.nix";
+          wantedBy = ["multi-user.target"];
+          after = ["network.target"];
+        };
+      }
     ];
   };
 }
