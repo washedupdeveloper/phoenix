a7a6a129a72efee467915fdd6e4254126b4472ae
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-02-01 17:55:37 +0100

    fixed typo

--- modules/flake/nixosConfigurations.nix
+++ modules/flake/nixosConfigurations.nix
@@ -48,9 +48,9 @@ in {
     ];
     liveISO = systemConfig "x86_64-linux" [
       {
-        services.openssh.settings.PasswordAuthentication = lib.mkForce true;
+        services.openssh.settings.PasswordAuthentication = inputs.nixpkgs.lib.mkForce true;
         users.users.root = {
-          initialHashedPassword = lib.mkForce "$y$j9T$f4LE30RF2QMBy6jiR5j3M1$/X6daMyAm0fJ9iohebi0LZjiCHrmK092WpBpdTW6Z7A";
+          initialHashedPassword = inputs.nixpkgs.lib.mkForce "$y$j9T$f4LE30RF2QMBy6jiR5j3M1$/X6daMyAm0fJ9iohebi0LZjiCHrmK092WpBpdTW6Z7A";
           openssh.authorizedKeys.keys = [self.sshPubKey];
           # TODO: add AGE secret for SOPS
         };
