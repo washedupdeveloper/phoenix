298a3de87650d69a576792c3916ec4e83836100f
Storm <31986015+washedupdeveloper@users.noreply.github.com>
2024-02-09 19:48:59 +0100

    replaced broken wayland/hypr land with minimal gnome

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  username,
+  ...
+}: {
   imports = [./hardware.nix];
 
   networking.hostName = "nixos-laptop";
@@ -7,6 +11,9 @@
     hashedPassword = "$y$j9T$lMC7hcwcJYLWzYc.dmo6P.$pKG/CXDe5UfI.zyDvoj1GefBUkYB3Et6xwxfCwlFlV8";
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBCMD78tzMBKjffq9l65ho/6SDUrZu2gXeA6EpU5U/l 31986015+washedupdeveloper@users.noreply.github.com"];
   };
+
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = username;
 
   boot = {
     loader = {
@@ -54,30 +61,42 @@
     jack.enable = true;
   };
 
-  programs.hyprland = {
-    enable = true;
-    enableNvidiaPatches = true;
-    xwayland.enable = true;
-  };
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-  environment = {
-    variables.WLR_NO_HARDWARE_CURSOR = "1";
-    sessionVariables = {
-      WLR_NO_HARDWARE_CURSOR = "1";
-      NIXOS_OZONE_WL = "1";
-    };
-    systemPackages = with pkgs; [
-      dunst
-      libnotify
-      swww
-      kitty
-      rofi-wayland
-      waybar
-      (
-        waybar.overrideAttrs (oldAttrs: {mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];})
-      )
-    ];
-  };
+  services.udev.packages = with pkgs; [kitty gnome.gnome-settings-daemon];
+  environment.systemPackages = with pkgs; [gnomeExtensions.appindicator];
+  environment.gnome.excludePackages =
+    (with pkgs; [gnome-photos gnome-tour])
+    ++ (with pkgs.gnome; [
+      baobab # disk usage analyzer
+      cheese # photo booth
+      eog # image viewer
+      epiphany # web browser
+      gedit # text editor
+      simple-scan # document scanner
+      totem # video player
+      yelp # help viewer
+      evince # document viewer
+      file-roller # archive manager
+      geary # email client
+      seahorse # password manager
+
+      # these should be self explanatory
+      gnome-calculator
+      gnome-calendar
+      gnome-characters
+      gnome-clocks
+      gnome-contacts
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-music
+      gnome-screenshot
+      gnome-system-monitor
+      gnome-weather
+      gnome-disk-utility
+      pkgs.gnome-connections
+    ]);
 }
